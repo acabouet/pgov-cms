2cfeb72941a0538e3695bf27ccd7aacdb3587aa5
Daniel <daniel.gading@civicactions.com>
2025-02-10 20:29:08 -0500

    Revert sidebar fix

--- src/frontend/components/view--goal-search.tsx
+++ src/frontend/components/view--goal-search.tsx
@@ -68,7 +68,7 @@ export default function GoalsSearchView({ filters, goals, total, description }: 
 
   const masonryBP = filtersOpen ? {350: 1, 750: 2, 1400: 3} : {350: 1, 750: 2, 1060: 3, 1400: 4};
   return (
-    <div className="view--goal-search">
+    <div>
       <div className="grid-row flex-column flex-align-center hero text-white padding-y-5">
         <h1 className="font-heading-2xl">{description}</h1>
         <p className="font-sans-lg margin-y-0">Smart Strategy. Strong Execution.</p>
